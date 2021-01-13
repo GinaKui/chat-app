5ebbf236e9e2784f274ef11f088cc7537905d3e8
ginakui <developerkui@outlook.com>
2021-01-13 09:56:19 -0500

    link NewContact and NewConversation modal to Sidebar

---
+++ client/src/components/NewConversation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewConversation() {
+  return (
+    <div>
+      new conversation
+    </div>
+  )
+}
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,14 +2,25 @@ import React, { useState } from 'react'
 import Tab from 'react-bootstrap/Tab'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 import Conversations from './Conversations'
 import Contacts from './Contacts'
+import NewConversation from './NewConversation'
+import NewContact from './NewContact'
 
 const CONVERSATIONS_KEY = "conversations";
 const CONTACTS_KEY = "contacts"
 
 export default function Sidebar({ id }) {
   const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const conversationIsOpen = activeKey === CONVERSATIONS_KEY;
+  const openModal = () => {
+    setModalIsOpen(true)
+  }
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
   return (
     <div className="d-flex flex-column" style={{ maxWidth: '300px' }}>
       <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
@@ -29,14 +40,19 @@ export default function Sidebar({ id }) {
           <Contacts />
         </Tab.Pane>
       </Tab.Content>
-
       <div className="border-top border-right">
         My ID: <span>{id}</span>
       </div>
-      <Button>
-        New {activeKey === CONTACTS_KEY ? 'Contact': 'Conversation'}
+      <Button onClick={openModal}>
+        New {conversationIsOpen ? 'Conversations' : 'Contact' }
       </Button>
       </Tab.Container>
+      <Modal show={modalIsOpen} onHide={closeModal}>
+        {conversationIsOpen ? 
+          <NewConversation closeModal={closeModal}/> : 
+          <NewContact closeModal={closeModal}/>
+        }
+      </Modal>
     </div>
   )
 }
